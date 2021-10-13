b9c2ad050567bf8163ccb4662ae15346a1ca4e23
SmaginSasha <87414980+SmaginSasha@users.noreply.github.com>
2021-10-13 11:16:52 +0500

    Добавил файл с кодом

--- main.py
+++ main.py
@@ -6,25 +6,25 @@ from urllib.parse import urlparse
 
 
 def shorten_link(headers, link):
-    shorten_link_url = "https://api-ssl.bitly.com/v4/shorten"
-    body = {"long_url": link}
+    shorten_link_url = 'https://api-ssl.bitly.com/v4/shorten'
+    body = {'long_url': link}
 
     short_link = requests.post(shorten_link_url, headers=headers, json=body)
     short_link.raise_for_status()
-    bitlink = short_link.json()["link"]
+    bitlink = short_link.json()['link']
     return bitlink
 
 
 def count_clicks(headers, netloc_and_path):
-    count_clicks_url = f"https://api-ssl.bitly.com/v4/bitlinks/{netloc_and_path}/clicks/summary"
+    count_clicks_url = f'https://api-ssl.bitly.com/v4/bitlinks/{netloc_and_path}/clicks/summary'
     response = requests.get(count_clicks_url, headers=headers)
     response.raise_for_status()
-    return response.json()["total_clicks"]
+    return response.json()['total_clicks']
 
 
 def is_bitlink(headers, netloc_and_path):
-    bitlink_url = f"https://api-ssl.bitly.com/v4/bitlinks/{netloc_and_path}"
-    headers = {"Authorization": f"Bearer {token}"}
+    bitlink_url = f'https://api-ssl.bitly.com/v4/bitlinks/{netloc_and_path}'
+    headers = {"Authorization": f'Bearer {token}'}
     response = requests.get(bitlink_url, headers=headers)
     return response.ok
 
@@ -35,20 +35,20 @@ def get_netloc_and_path(link):
     return netloc_and_path
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     load_dotenv()
     parser = argparse.ArgumentParser(
         description='Сокращение ссылок'
     )
     parser.add_argument('link', help='Описание аргумента')
     link = parser.parse_args().link
-    token = os.getenv("BITLY_TOKEN")
-    headers = {"Authorization": f"Bearer {token}"}
+    token = os.getenv('BITLY_TOKEN')
+    headers = {'Authorization': f'Bearer {token}'}
     netloc_and_path = get_netloc_and_path(link)
     if is_bitlink(headers, netloc_and_path):
-        print("Кликов по ссылке", count_clicks(headers, netloc_and_path))
+        print('Кликов по ссылке', count_clicks(headers, netloc_and_path))
     else:
         try:
-            print("Битлинк",shorten_link(headers, link))
+            print('Битлинк',shorten_link(headers, link))
         except requests.exceptions.HTTPError:
-            print("Ваша ссылка неверна")
+            print('Ваша ссылка неверна')
